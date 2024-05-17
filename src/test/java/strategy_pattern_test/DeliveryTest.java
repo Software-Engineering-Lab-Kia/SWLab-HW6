594f7f3ba67021784e367917f26c4ef38a6ed8dd
Amirreza <amirreza.bagheri7680@gmail.com>
2024-05-17 12:40:10 +0330

    Tests for Strategy and State

--- src/test/java/strategy_pattern_test/DeliveryTest.java
+++ src/test/java/strategy_pattern_test/DeliveryTest.java
@@ -1,8 +1,12 @@
 package strategy_pattern_test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import org.junit.jupiter.api.Test;
+
+import state_pattern.DeliveredState;
+import state_pattern.TransitState;
 import strategy_pattern.DeliveryMethod;
 import strategy_pattern.ExpressDelivery;
 import strategy_pattern.Package;
@@ -10,7 +14,7 @@ import strategy_pattern.StandardDelivery;
 
 public class DeliveryTest {
     @Test
-    void deliveryPrinceTest() {
+    void deliveryPriceTest() {
         Package deliveryPackage = new Package(10);
 
         DeliveryMethod standardDelivery = new StandardDelivery();
@@ -20,18 +24,34 @@ public class DeliveryTest {
         assertEquals(expressDelivery.deliveryPrice(deliveryPackage.getWeight()), 35);
     }
 
-    @Test
-    void simpleStandardDeliveryTest() {
 
+    @Test
+    void strategyTest(){
+        Package package1 = new Package(10);
+
+        StandardDelivery standardDelivery = new StandardDelivery();
+        package1.setDeliveryMethod(standardDelivery);
+
+        assertInstanceOf(StandardDelivery.class, package1.getDeliveryMethod());
+        assertEquals(package1.getPrice(), 25);
+
+        ExpressDelivery expressDelivery = new ExpressDelivery();
+        package1.setDeliveryMethod(expressDelivery);
+        assertInstanceOf(ExpressDelivery.class, package1.getDeliveryMethod());
+        assertEquals(package1.getPrice(), 35);
     }
 
     @Test
-    void simpleExpressDeliveryTest() {
+    void stateTest(){
+        Package package1 = new Package(10);
 
-    }
+        TransitState transitState = new TransitState();
+        package1.setState(transitState);
 
-    @Test
-    void changeDeliveryMethodTest() {
+        assertInstanceOf(TransitState.class, package1.getState());
 
+        DeliveredState deliveredState = new DeliveredState();
+        package1.setState(deliveredState);
+        assertInstanceOf(DeliveredState.class, package1.getState());
     }
 }
